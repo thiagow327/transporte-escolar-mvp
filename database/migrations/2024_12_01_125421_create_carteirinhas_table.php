2868be92fb75c54f4dc12f7601507c714f0dc0a5
thiagosilva327@live.com <thiago@Jarvis>
2024-12-01 09:59:15 -0300

    feat: config migrations

--- database/migrations/2024_12_01_125421_create_carteirinhas_table.php
+++ database/migrations/2024_12_01_125421_create_carteirinhas_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('carteirinhas', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('aluno_id')->constrained()->onDelete('cascade');
+            $table->boolean('ativo')->default(true);
+            $table->date('data_validade');
             $table->timestamps();
         });
     }
--- database/migrations/2024_12_01_125446_create_pagamentos_table.php
+++ database/migrations/2024_12_01_125446_create_pagamentos_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('pagamentos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('carteirinha_id')->constrained()->onDelete('cascade');
+            $table->date('data_pagamento');
+            $table->decimal('valor', 8, 2);
+            $table->string('recebedor');
+            $table->string('tipo_pagamento');
+            $table->text('observacoes')->nullable();
             $table->timestamps();
         });
     }
