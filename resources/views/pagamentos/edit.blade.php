a4102e520e485c6f2a158fc7f8bc0c1e0749d9a4
thiagosilva327@live.com <thiago@Jarvis>
2024-12-01 12:18:35 -0300

    refactoring: remove field empty and correct style for pagamentos view

--- resources/views/pagamentos/edit.blade.php
+++ resources/views/pagamentos/edit.blade.php
@@ -1,50 +1,64 @@
 <x-app-layout>
-    <div class="container mt-4">
-        <h2>Editar Pagamento</h2>
-        <form method="POST" action="{{ route('pagamentos.update', $pagamento->id) }}">
-            @csrf
-            @method('PUT')
-            <div class="mb-3">
-                <label for="carteirinha_id" class="form-label">Carteirinha</label>
-                <select class="form-select" id="carteirinha_id" name="carteirinha_id" required>
-                    <option value="" disabled selected>Selecione uma carteirinha</option>
-                    @foreach($carteirinhas as $carteirinha)
-                        <option value="{{ $carteirinha->id }}"
-                                @if($carteirinha->id == $pagamento->carteirinha_id) selected @endif>{{ $carteirinha->aluno->nome }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="data_pagamento" class="form-label
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Editar Pagamento') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="container mt-4">
+                    <form method="POST" action="{{ route('pagamentos.update', $pagamento->id) }}">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="carteirinha_id" class="form-label">Carteirinha</label>
+                            <select class="form-select" id="carteirinha_id" name="carteirinha_id" required>
+                                <option value="" disabled selected>Selecione uma carteirinha</option>
+                                @foreach($carteirinhas as $carteirinha)
+                                    <option value="{{ $carteirinha->id }}"
+                                            @if($carteirinha->id == $pagamento->carteirinha_id) selected @endif>{{ $carteirinha->aluno->nome }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="data_pagamento" class="form-label
                 ">Data de Pagamento</label>
-                <input type="date" class="form-control" id="data_pagamento" name="data_pagamento"
-                       value="{{ $pagamento->data_pagamento }}" required>
-            </div>
-            <div class="mb-3">
-                <label for="valor" class="form-label">Valor</label>
-                <input type="text" class="form-control" id="valor" name="valor" value="{{ $pagamento->valor }}"
-                       required>
-            </div>
-            <div class="mb-3">
-                <label for="recebedor" class="form-label
+                            <input type="date" class="form-control" id="data_pagamento" name="data_pagamento"
+                                   value="{{ $pagamento->data_pagamento }}" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="valor" class="form-label">Valor</label>
+                            <input type="text" class="form-control" id="valor" name="valor"
+                                   value="{{ $pagamento->valor }}"
+                                   required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="recebedor" class="form-label
                 ">Recebedor</label>
-                <input type="text" class="form-control" id="recebedor" name="recebedor"
-                       value="{{ $pagamento->recebedor }}"
-                       required>
-            </div>
-            <div class="mb-3">
-                <label for="tipo_pagamento" class="form-label
+                            <input type="text" class="form-control" id="recebedor" name="recebedor"
+                                   value="{{ $pagamento->recebedor }}"
+                                   required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="tipo_pagamento" class="form-label
                 ">Tipo de Pagamento</label>
-                <input type="text" class="form-control" id="tipo_pagamento" name="tipo_pagamento"
-                       value="{{ $pagamento->tipo_pagamento }}" required>
+                            <input type="text" class="form-control" id="tipo_pagamento" name="tipo_pagamento"
+                                   value="{{ $pagamento->tipo_pagamento }}" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="observacoes" class="form-label">Observações</label>
+                            <textarea class="form-control" id="observacoes" name="observacoes"
+                                      required>{{ $pagamento->observacoes }}</textarea>
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-success">Salvar</button>
+                            <a href="{{ route('pagamentos.index') }}" class="btn btn-secondary">Voltar</a>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="observacoes" class="form-label">Observações</label>
-                <textarea class="form-control" id="observacoes" name="observacoes"
-                          required>{{ $pagamento->observacoes }}</textarea>
-            </div>
-            <button type="submit" class="btn btn-success">Salvar</button>
-            <button type="button" class="btn btn-secondary" onclick="window.history.back()">Cancelar</button>
-        </form>
+        </div>
     </div>
 </x-app-layout>
--- resources/views/pagamentos/show.blade.php
+++ resources/views/pagamentos/show.blade.php
@@ -1,40 +1,35 @@
 <x-app-layout>
-    <div class="container mt-4">
-        <h2>Detalhes do Pagamento</h2>
-        <div class="mb-3">
-            <label for="carteirinha_id" class="form-label">Carteirinha</label>
-            <input type="text" class="form-control" id="carteirinha_id" name="carteirinha_id"
-                   value="{{ $pagamento->carteirinha->aluno->nome }}" readonly>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Detalhes do Pagamento') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="container mt-4">
+                    <div class="mb-3">
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <strong>Carteirinha:</strong> {{ $pagamento->carteirinha->aluno->nome }}</li>
+                            <li class="list-group-item"><strong>Data de
+                                    Pagamento:</strong> {{ $pagamento->data_pagamento }}</li>
+                            <li class="list-group-item"><strong>Valor:</strong>
+                                R$ {{ number_format($pagamento->valor, 2, ',', '.') }}</li>
+                            <li class="list-group-item"><strong>Recebedor:</strong> {{ $pagamento->recebedor }}</li>
+                            <li class="list-group-item"><strong>Tipo de
+                                    Pagamento:</strong> {{ $pagamento->tipo_pagamento }}</li>
+                            <li class="list-group-item"><strong>Observações:</strong> {{ $pagamento->observacoes }}</li>
+                        </ul>
+                    </div>
+                    <div class="mb-3">
+                        <a href="{{ route('pagamentos.edit', $pagamento->id) }}" class="btn btn-primary">Editar</a>
+                        <a href="{{ route('pagamentos.index') }}" class="btn btn-secondary">Voltar</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="data_pagamento" class="form-label
-            ">Data de Pagamento</label>
-            <input type="text" class="form-control" id="data_pagamento" name="data_pagamento"
-                   value="{{ $pagamento->data_pagamento }}" readonly>
-        </div>
-        <div class="mb-3">
-            <label for="valor" class="form-label">Valor</label>
-            <input type="text" class="form-control" id="valor" name="valor" value="{{ $pagamento->valor }}" readonly>
-        </div>
-        <div class="mb-3">
-            <label for="recebedor" class="form-label
-            ">Recebedor</label>
-            <input type="text" class="form-control" id="recebedor" name="recebedor" value="{{ $pagamento->recebedor }}"
-                   readonly>
-        </div>
-        <div class="mb-3">
-            <label for="tipo_pagamento" class="form-label
-            ">Tipo de Pagamento</label>
-            <input type="text" class="form-control" id="tipo_pagamento" name="tipo_pagamento"
-                   value="{{ $pagamento->tipo_pagamento }}" readonly>
-        </div>
-        <div class="mb-3">
-            <label for="observacoes" class="form-label">Observações</label>
-            <textarea class="form-control" id="observacoes" name="observacoes"
-                      readonly>{{ $pagamento->observacoes }}</textarea>
-        </div>
-        <a href="{{ route('pagamentos.edit', $pagamento->id) }}" class="btn btn-primary">Editar</a>
-        <a href="{{ route('pagamentos.index') }}" class="btn btn-secondary">Voltar</a>
     </div>
 </x-app-layout>
 
