d5d9d8b7789f341d5551aba1778fae5997510c10
thiagosilva327@live.com <thiago@Jarvis>
2024-12-01 10:01:59 -0300

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('alunos', AlunoController::class);
+    Route::resource('carteirinhas', CarteirinhaController::class);
+    Route::resource('pagamentos', PagamentoController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
