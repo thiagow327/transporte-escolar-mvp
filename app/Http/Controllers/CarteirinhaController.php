d62be6c787e10d19bb2cb384485dec71ba44f3c0
thiagosilva327@live.com <thiago@Jarvis>
2024-12-01 09:54:35 -0300

    feat: create model, controller and migration for carteirinha

---
+++ app/Http/Controllers/CarteirinhaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Carteirinha;
+use Illuminate\Http\Request;
+
+class CarteirinhaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Carteirinha $carteirinha)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Carteirinha $carteirinha)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Carteirinha $carteirinha)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Carteirinha $carteirinha)
+    {
+        //
+    }
+}
