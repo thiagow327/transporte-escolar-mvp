d4e6d776e832c79a5ff946a8ca6801333c03f61a
Thiago Gomes da Silva <thiagosilva327@live.com>
2024-12-06 18:19:47 -0300

    feat: style view for pagamentos

--- resources/views/pagamentos/create.blade.php
+++ resources/views/pagamentos/create.blade.php
@@ -11,6 +11,7 @@
                 <div class="container mt-4">
                     <form method="POST" action="{{ route('pagamentos.store') }}">
                         @csrf
+
                         <div class="mb-3">
                             <label for="carteirinha_id" class="form-label">Carteirinha</label>
                             <select class="form-select" id="carteirinha_id" name="carteirinha_id" required>
@@ -20,25 +21,35 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="mb-3">
-                            <label for="data_pagamento" class="form-label">Data de Pagamento</label>
-                            <input type="date" class="form-control" id="data_pagamento" name="data_pagamento"
-                                   value="{{ old('data_pagamento') }}" required>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="data_pagamento" class="form-label">Data de Pagamento</label>
+                                <input type="date" class="form-control" id="data_pagamento" name="data_pagamento"
+                                       value="{{ old('data_pagamento') }}" required>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="valor" class="form-label">Valor</label>
+                                <input type="text" class="form-control" id="valor" name="valor"
+                                       value="{{ old('valor') }}"
+                                       required>
+                            </div>
                         </div>
-                        <div class="mb-3">
-                            <label for="valor" class="form-label">Valor</label>
-                            <input type="text" class="form-control" id="valor" name="valor" value="{{ old('valor') }}"
-                                   required>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="recebedor" class="form-label">Recebedor</label>
+                                <input type="text" name="recebedor" id="recebedor" class="form-control" required>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="tipo_pagamento" class="form-label">Tipo de Pagamento</label>
+                                <select class="form-select" id="tipo_pagamento" name="tipo_pagamento" required>
+                                    <option value="Pix">Pix</option>
+                                    <option value="Cartão de Crédito">Cartão de Crédito</option>
+                                    <option value="Cartão de Débito">Cartão de Débito</option>
+                                    <option value="Dinheiro">Dinheiro</option>
+                                </select>
+                            </div>
                         </div>
-                        <div class="mb-3">
-                            <label for="recebedor" class="form-label">Recebedor</label>
-                            <input type="text" name="recebedor" id="recebedor" class="form-control" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="tipo_pagamento" class="form-label">Tipo de Pagamento</label>
-                            <input type="text" class="form-control" id="tipo_pagamento" name="tipo_pagamento"
-                                   value="{{ old('tipo_pagamento') }}" required>
-                        </div>
+
                         <div class="mb-3">
                             <label for="observacoes" class="form-label">Observações</label>
                             <textarea class="form-control" id="observacoes"
