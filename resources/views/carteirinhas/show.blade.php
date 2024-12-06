57ce8b3bc3213fc2daa01012ed1578605af12756
Thiago Gomes da Silva <thiagosilva327@live.com>
2024-12-06 17:56:07 -0300

    feat: join aluno with carteirinha in show view

--- resources/views/carteirinhas/show.blade.php
+++ resources/views/carteirinhas/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Detalhes do Aluno') }}
+            {{ __('Detalhes da Carteirinha') }}
         </h2>
     </x-slot>
 
@@ -9,17 +9,39 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="container mt-4">
-                    <h4 class="mt-4">Dados</h4>
-                    <ul class="list-group">
-                        <li class="list-group-item"><strong>Aluno:</strong> {{ $carteirinha->aluno->nome }}</li>
-                        <li class="list-group-item"><strong>Ativo:</strong> {{ $carteirinha->ativo ? 'Sim' : 'Não' }}
-                        </li>
-                        <li class="list-group-item"><strong>Dia de
-                                Vencimento:</strong> {{ $carteirinha->vencimento_dia }}</li>
-                        </li>
-                    </ul>
+                    <h3>Informações do Aluno</h3>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <p><strong>Nome:</strong> {{ $carteirinha->aluno->nome }}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p><strong>Idade:</strong> {{ $carteirinha->aluno->idade }}</p>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <p><strong>Responsável:</strong> {{ $carteirinha->aluno->responsavel }}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p><strong>Contato do Responsável:</strong> {{ $carteirinha->aluno->contato_responsavel }}
+                            </p>
+                        </div>
+                    </div>
 
-                    <h4 class="mt-8">Pagamentos</h4>
+                    <h3>Informações da Carteirinha</h3>
+                    <div class="row mb-3">
+                        <div class="col-md-3">
+                            <p><strong>Dia de Vencimento:</strong> {{ $carteirinha->vencimento_dia }}</p>
+                        </div>
+                        <div class="col-md-3">
+                            <p><strong>Escola:</strong> {{ $carteirinha->escola }}</p>
+                        </div>
+                        <div class="col-md-3">
+                            <p><strong>Horário:</strong> {{ $carteirinha->horario }}</p>
+                        </div>
+                    </div>
+
+                    <h3 class="mt-8">Pagamentos</h3>
                     @if($carteirinha->pagamentos->isEmpty())
                         <p>Nenhum pagamento registrado.</p>
                     @else
