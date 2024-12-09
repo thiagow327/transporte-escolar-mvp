f5167d97db330dbd2ce112c77ba5a5f6c81ad186
thiagosilva327@live.com <thiago@Jarvis>
2024-12-08 22:48:19 -0300

    feat: add softdeletes in model aluno, pagamento and carteirinha

--- app/Models/Carteirinha.php
+++ app/Models/Carteirinha.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Carteirinha extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'aluno_id',
         'vencimento_dia',
