e743f6b0f9ace4e2d1fd1e106b936c8ffef0113c
thiagosilva327@live.com <thiago@Jarvis>
2024-12-14 22:17:13 -0300

    feat: add news schools in filter and button clean filter

--- resources/views/carteirinhas/index.blade.php
+++ resources/views/carteirinhas/index.blade.php
@@ -23,9 +23,58 @@
                             <div class="col-md-3">
                                 <select name="escola" class="form-select">
                                     <option value="">Filtrar por escola</option>
-                                    <option value="a" {{ request('escola') == 'a' ? 'selected' : '' }}>A</option>
-                                    <option value="b" {{ request('escola') == 'b' ? 'selected' : '' }}>B</option>
-                                    <option value="c" {{ request('escola') == 'c' ? 'selected' : '' }}>C</option>
+                                    <option
+                                        value="CCA - Todos Irmãos" {{ request('escola') == 'CCA - Todos Irmãos' ? 'selected' : '' }}>
+                                        CCA - Todos Irmãos
+                                    </option>
+                                    <option
+                                        value="CCA - Ana Maria" {{ request('escola') == 'CCA - Ana Maria' ? 'selected' : '' }}>
+                                        CCA - Ana Maria
+                                    </option>
+                                    <option
+                                        value="CEI - Castelo Branco" {{ request('escola') == 'CEI - Castelo Branco' ? 'selected' : '' }}>
+                                        CEI - Castelo Branco
+                                    </option>
+                                    <option
+                                        value="CEI - Novo Amanhecer" {{ request('escola') == 'CEI - Novo Amanhecer' ? 'selected' : '' }}>
+                                        CEI - Novo Amanhecer
+                                    </option>
+                                    <option
+                                        value="CEI - Pequeno Anjo" {{ request('escola') == 'CEI - Pequeno Anjo' ? 'selected' : '' }}>
+                                        CEI - Pequeno Anjo
+                                    </option>
+                                    <option
+                                        value="CEI - Pingo de Orvalho" {{ request('escola') == 'CEI - Pingo de Orvalho' ? 'selected' : '' }}>
+                                        CEI - Pingo de Orvalho
+                                    </option>
+                                    <option
+                                        value="CEI - Vem ser feliz" {{ request('escola') == 'CEI - Vem ser feliz' ? 'selected' : '' }}>
+                                        CEI - Vem ser feliz
+                                    </option>
+                                    <option
+                                        value="CEI - Vila Nina" {{ request('escola') == 'CEI - Vila Nina' ? 'selected' : '' }}>
+                                        CEI - Vila Nina
+                                    </option>
+                                    <option
+                                        value="EMEI - Tito Lívio Ferreira" {{ request('escola') == 'EMEI - Tito Lívio Ferreira' ? 'selected' : '' }}>
+                                        EMEI - Tito Lívio Ferreira
+                                    </option>
+                                    <option
+                                        value="EMF - Raul Fernandes" {{ request('escola') == 'EMF - Raul Fernandes' ? 'selected' : '' }}>
+                                        EMF - Raul Fernandes
+                                    </option>
+                                    <option
+                                        value="EMF - Geraldo Sessor Junior" {{ request('escola') == 'EMF - Geraldo Sessor Junior' ? 'selected' : '' }}>
+                                        EMF - Geraldo Sessor Junior
+                                    </option>
+                                    <option
+                                        value="EMF - Galdino Lopes Chagas" {{ request('escola') == 'EMF - Galdino Lopes Chagas' ? 'selected' : '' }}>
+                                        EMF - Galdino Lopes Chagas
+                                    </option>
+                                    <option
+                                        value="Colégio Roldhum" {{ request('escola') == 'Colégio Roldhum' ? 'selected' : '' }}>
+                                        Colégio Roldhum
+                                    </option>
                                 </select>
                             </div>
 
@@ -37,12 +86,16 @@
                                     </option>
                                     <option value="tarde" {{ request('horario') == 'tarde' ? 'selected' : '' }}>Tarde
                                     </option>
+                                    <option value="integral" {{ request('horario') == 'integral' ? 'selected' : '' }}>
+                                        Integral
+                                    </option>
                                 </select>
                             </div>
 
-                            <!-- Botão de filtro -->
+                            <!-- Botão de filtro e limpar filtros -->
                             <div class="col-md-3">
                                 <button type="submit" class="btn btn-primary">Filtrar</button>
+                                <a href="{{ route('carteirinhas.index') }}" class="btn btn-secondary">Limpar</a>
                             </div>
                         </div>
                     </form>
