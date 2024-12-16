ae5fd959972f8ceefffa3431f600afd0d5b16d18
Thiago Gomes da Silva <thiagosilva327@live.com>
2024-12-16 11:00:22 -0300

    refactor: remove methods not used in aluno controller

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -7,33 +7,6 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Request $request)
-    {
-        $query = Aluno::query();
-
-        if ($request->has('search') && $request->search) {
-            $query->where('nome', 'LIKE', "%{$request->search}%");
-        }
-
-        $alunos = $query->paginate(10);
-
-        return view('alunos.index', compact('alunos'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('alunos.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -46,25 +19,6 @@ class AlunoController extends Controller
         return redirect()->route('alunos.index')->with('success', 'Aluno cadastrado com sucesso!');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Aluno $aluno)
-    {
-        return view('alunos.show', compact('aluno'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Aluno $aluno)
-    {
-        return view('alunos.edit', compact('aluno'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Aluno $aluno)
     {
         $request->validate([
@@ -77,9 +31,6 @@ class AlunoController extends Controller
         return redirect()->route('alunos.index')->with('success', 'Aluno atualizado com sucesso!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Aluno $aluno)
     {
         $aluno->delete();
