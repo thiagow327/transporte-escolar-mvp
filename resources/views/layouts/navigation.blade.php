ec75b3ccc36bef4bc883ff8bf89d87ae5dc8d7d4
thiagosilva327@live.com <thiago@Jarvis>
2024-12-26 11:19:18 -0300

    feat: add icon tgsoft logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"/>
+                        <img src="{{ asset('images/logo_tgsoft.svg') }}" class="block h-9 w-auto"
+                             alt="Application Logo">
                     </a>
                 </div>
 
