3117ec4a000d1e4c129ad4e226159b4c71529b95
thiagosilva327@live.com <thiago@Jarvis>
2024-12-31 14:45:11 -0300

    feat: add chanel

--- config/logging.php
+++ config/logging.php
@@ -54,7 +54,7 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => explode(',', env('LOG_STACK', 'single')),
+            'channels' => explode(',', env('LOG_STACK', 'single', 'bugsnag')),
             'ignore_exceptions' => false,
         ],
 
@@ -125,6 +125,10 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+
+        'bugsnag' => [
+            'driver' => 'bugsnag',
         ],
 
     ],
