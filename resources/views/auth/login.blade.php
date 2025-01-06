00db90ce740f139b898f37bf57854c7307c75cac
thiagosilva327@live.com <thiago@Jarvis>
2025-01-06 06:42:09 -0300

    fix: translate password field

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
 
         <!-- Password -->
         <div class="mt-4 relative">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Senha')" />
 
             <div class="relative">
                 <x-text-input id="password" class="block mt-1 w-full pr-10"
